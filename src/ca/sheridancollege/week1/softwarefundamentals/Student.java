51baea08ee4b3c2a79154d5cca85ec22c2974ebe
smarceta <marcetas@sheridancollege.ca>
2020-01-16 14:55:56 -0500

    hi

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -1,34 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ca.sheridancollege.week1.softwarefundamentals;
 
-/**
- *
- * @author sidhu376
- */
+
 public class Student {
     
     private String name;
+    private int id;
+    private String program;
 
-    /**
-     * Get the value of name
-     *
-     * @return the value of name
-     */
+    
+    
     public String getName() {
         return name;
     }
 
-    /**
-     * Set the value of name
-     *
-     * @param name new value of name
-     */
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
     }
 
 }
