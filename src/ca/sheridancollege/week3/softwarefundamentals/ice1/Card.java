4d13f1f4186696153c2a01dbf722c4dd698d29a2
smarceta <marcetas@sheridancollege.ca>
2020-01-23 15:22:01 -0500

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,4 +1,5 @@
-/*
+/* * SANJIN MARCETA
+ * 991574394
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,7 +13,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author
+ *  * SANJIN MARCETA
+ * 991574394
  */
 public class Card {
 
@@ -47,7 +49,23 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
+    public int RandomNumb() {
+        int max = 13; 
+        int min = 1; 
+        int range = max - min + 1; 
+        int rand = (int)(Math.random() * range) + min;
+        return rand; 
+        // generate random numbers within 1 to 10 
+    }
+    public int RandomNu() {
+        int max = 3; 
+        int min = 0; 
+        int range = max - min + 1; 
+        int rrand = (int)(Math.random() * range) + min;
+        return rrand; 
+        // generate random numbers within 1 to 10 
+    }
+    }
    
    
     
-}
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,30 +1,59 @@
-/*
+/* * SANJIN MARCETA
+ * 991574394
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Scanner; 
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author 
+ * SANJIN MARCETA
+ * 991574394
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         Card[] magicHand = new Card[7];
+        
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(c.RandomNumb());
+            c.setSuit(Card.SUITS[c.RandomNu()]);
         }
         
-        //insert code to ask the user for Card value and suit, create their card
+        
+        
+        System.out.println("Enter Card Value");
+        int cardNu = input.nextInt(); 
+        
+        for(int i = 0; i<7; i++){
+            if(Card[i] = cardNu) {
+              System.out.println("COrrect Number");
+            }else System.out.println("Nope!");
+        }
+        
+        
+        
+        
+        System.out.println("Enter Suit");
+        String cardSu = input.nextLine();
+        
+        for(int i = 0; i<4; i++){
+            if(Card.SUITS[i] == cardSu){
+                System.out.println(cardSu + "is correct!");
+                
+            }else System.out.println("Not correct");
+        }
+        
+//insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
     }
