54e3be91b6a422076f336806d1a340113d5893fb
smarceta <59974538+smarceta@users.noreply.github.com>
2020-01-23 15:37:15 -0500

    Update CardTrick.java
     Hard-coded luckyCard

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * SANJIN MARCETA
- * 991574394
+ * 991574394////
  */
 public class CardTrick {
     
